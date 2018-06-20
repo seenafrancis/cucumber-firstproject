d75a891bd49631473c60f68c1f2440827ac7e0f4
Sankar Murugan <seena.nunu@gmail.com>
2018-06-20 21:43:01 +1000

    test commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+gem 'selenium-webdriver', '3.4.3'
+gem 'rspec'
+gem 'cucumber', '3.1.0'
---
+++ features/step_definitions/test.rb
@@ -0,0 +1,13 @@
+class Sample
+    def hello
+       puts "Hello Ruby!"
+    end
+    def helloseena
+        puts "Hello seena!"
+     end
+ end
+ 
+ # Now using above class to create objects
+ object = Sample. new
+ object.hello
+ object.helloseena
